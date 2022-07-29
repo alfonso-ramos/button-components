33752ff2f8310fccceb0abe7623bf6a8fb866b76
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2022-07-28 20:00:02 -0600

    [ADD] Primer commit, componente Button creado

---
+++ Documents/Projects/button-component/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./components/Button"
+import Layout from "./containers/Layout"
+
+function App() {
+  return (
+    <Layout>
+      <Button />
+    </Layout>
+  )
+}
+
+export default App
